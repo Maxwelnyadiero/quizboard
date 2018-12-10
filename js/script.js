04a4d348770eaf50b8f6471ac9870e731f32f4aa
rosemarymagoma99@gmail.com <maxwelnyadiero@gmail.com>
2018-12-10 08:07:34 +0300

    added div class

---
+++ js/script.js
@@ -0,0 +1,14 @@
+(function(){
+var questions = [{
+  question:"what is 15*7?",
+  choices: [100, 105, 125, 115],
+  correctAnswer:
+  1
+}, {
+  question:
+  "what is 20*5?",
+  choices: [50, 60, 102, 100],
+  correctAnswer:
+  3
+}
+}]
