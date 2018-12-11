7dc931179fb3fdf4d67fea5f1489b6951ef76cda
rosemarymagoma99@gmail.com <maxwelnyadiero@gmail.com>
2018-12-11 07:24:28 +0300

    added document function

--- js/script.js
+++ js/script.js
@@ -38,3 +38,14 @@ var testResults = function(one, two ,three, four, five){
 
 
 });
+$(document).ready(function(){
+  $("button#green").click(function(){
+    $("body").addClass("green-background");
+  });
+  $("button#yellow").click(function(){
+    $("body").addClass("yellow-background");
+  });
+  $("button#red").click(function(){
+    $("body").addClass("red-background"));
+  });
+});
